5b325b4cb49b2d65ddbe2b066bd7b19e90cd0ae3
TCLProject <62664720+TCLProject@users.noreply.github.com>
2023-01-02 21:11:33 +0000

    Removed clientside requirement

--- src/main/java/net/tclproject/teresetfix/TEResetFix.java
+++ src/main/java/net/tclproject/teresetfix/TEResetFix.java
@@ -4,11 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = TEResetFix.MODID, version = TEResetFix.VERSION, name = TEResetFix.NAME)
+@Mod(modid = TEResetFix.MODID, version = TEResetFix.VERSION, name = TEResetFix.NAME, acceptableRemoteVersions = "*")
 public class TEResetFix {
     public static final String MODID = "teresetfix";
     public static final String NAME = "TileEntity Reset Fix";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.1-noreq";
 
     public static final Logger logger = LogManager.getLogger("TEResetFix");
 }
